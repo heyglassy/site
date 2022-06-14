ef0caa18767eeddd585cb8becd59400f7897cdfd
Christian Glassiognon <christian@heyglassy.co>
2022-06-14 05:24:16 -0700

    Added name and socials

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#191919",
+      },
+    },
+  },
+  plugins: [],
+};

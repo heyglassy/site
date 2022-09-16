ac86a6a1c4dda389b1c3ee8e85c7fe2aa4a4d1ba
Christian Glassiognon <christian@heyglassy.co>
2022-09-16 00:28:20 -0700

    fixed rotation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
     theme: {
@@ -5,6 +6,12 @@ module.exports = {
             colors: {
                 primary: "#020C00",
             },
+        },
+        fontFamily: {
+            serif: ["Quando", "ui-serif", "Georgia"],
+        },
+        animation: {
+            "spin-slow": "spin 20s linear infinite",
         },
     },
     plugins: [],
